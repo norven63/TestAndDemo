c68bda5cd00289650cdcd11d07167cda10dbc5c6
norven63 <norven63@163.com>
2014-05-15 10:49:28 +0800

    1

--- TestAndDemo/src/test/Test.java
+++ TestAndDemo/src/test/Test.java
@@ -1,5 +1,8 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
 	public static void main(String[] args) {
 		String string = "a.b.c";
@@ -7,5 +10,14 @@ public class Test {
 		for (String item : strings) {
 			System.out.println(item);
 		}
+
+		List list = new ArrayList();
+		System.out.println(list.size());
+		aaa(list);
+		System.out.println(list.size());
+	}
+
+	public static void aaa(List s) {
+		s.add("1");
 	}
 }
