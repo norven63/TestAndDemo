829b93127554149663fba53b020874a8c0a7525e
Norven <norven63@gmail.com>
2015-07-16 17:53:26 +0800

    1

--- TestAndDemo/src/demo/TestDemo.java
+++ TestAndDemo/src/demo/TestDemo.java
@@ -5,18 +5,35 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 
+import com.google.gson.Gson;
+
 public class TestDemo {
 	public static void main(String[] args) {
-		Calendar calendar = Calendar.getInstance();
-		calendar.set(Calendar.WEEK_OF_MONTH, 2015);
-		calendar.set(Calendar.MONTH, 0);
-		calendar.set(Calendar.DAY_OF_MONTH, 1);
-		System.out.println(calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.CHINESE));
-		System.out.println(calendar.get(Calendar.HOUR_OF_DAY));
+		// Calendar calendar = Calendar.getInstance();
+		// calendar.set(Calendar.WEEK_OF_MONTH, 2015);
+		// calendar.set(Calendar.MONTH, 0);
+		// calendar.set(Calendar.DAY_OF_MONTH, 1);
+		// System.out.println(calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT,
+		// Locale.CHINESE));
+		// System.out.println(calendar.get(Calendar.HOUR_OF_DAY));
 
 		// A a = new A();
 		// List<A> list = a.testA(a);// 这里声明list时，所用的A即会给下面的testA方法第一个V赋值
 		// System.out.println(list.get(0));
+		
+		String a = "{\"a\":\"abc\",\"b\":2}";
+
+		System.out.println(new Gson().fromJson(a, B.class));
+	}
+
+	static class B {
+		private String a = "yyy";
+		private int b = 9999;
+
+		@Override
+		public String toString() {
+			return "B [a=" + a + ", b=" + b + "]";
+		}
 	}
 
 	static class A {
