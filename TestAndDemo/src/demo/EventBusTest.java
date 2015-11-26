cf89a4267639f00aef53a746331772a00bb31021
zhouxinxing <zhouxinxing@mobimagic.com>
2015-11-26 14:32:58 +0800

    1

--- TestAndDemo/src/demo/EventBusTest.java
+++ TestAndDemo/src/demo/EventBusTest.java
@@ -4,37 +4,37 @@ import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
 
 public class EventBusTest {
-	public static void main(String[] args) {
-		EventBus bus = new EventBus();
-		EventBus bus2 = new EventBus();
+    public static void main(String[] args) {
+        EventBus bus = new EventBus();
+        EventBus bus2 = new EventBus();
 
-		IntegerListenr1 listenr1 = new IntegerListenr1();
-		IntegerListenr2 listenr2 = new IntegerListenr2();
+        IntegerListenr1 listenr1 = new IntegerListenr1();
+        IntegerListenr2 listenr2 = new IntegerListenr2();
 
-		bus.register(listenr1);
-		bus.register(listenr2);
-		bus2.register(listenr2);
+        bus.register(listenr1);
+        bus.register(listenr2);
+        bus2.register(listenr2);
 
-		bus.post(2);
-		bus2.post(3);
-	}
+        bus.post(2);
+        bus2.post(3);
+    }
 
-	public static class IntegerListenr1 {
-		@Subscribe
-		public void handler1(Integer event) {
-			System.out.println("listener1_handler1: " + event);
-		}
+    public static class IntegerListenr1 {
+        @Subscribe
+        public void handler1(Integer event) {
+            System.out.println("listener1_handler1: " + event);
+        }
 
-		@Subscribe
-		public void handler2(Integer event) {
-			System.out.println("listener1_handler2: " + event);
-		}
-	}
+        @Subscribe
+        public void handler2(Integer event) {
+            System.out.println("listener1_handler2: " + event);
+        }
+    }
 
-	public static class IntegerListenr2 {
-		@Subscribe
-		public void handler(Integer event) {
-			System.out.println("listener2: " + event);
-		}
-	}
+    public static class IntegerListenr2 {
+        @Subscribe
+        public void handler(Integer event) {
+            System.out.println("listener2: " + event);
+        }
+    }
 }
--- TestAndDemo/src/demo/TestDemo.java
+++ TestAndDemo/src/demo/TestDemo.java
@@ -8,43 +8,47 @@ import java.util.List;
 import com.google.gson.Gson;
 
 public class TestDemo {
-	public static void main(String[] args) {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy年 MM月 dd日");
-		System.out.println(sdf.format(new Date()));
-		
-		// Calendar calendar = Calendar.getInstance();
-		// calendar.set(Calendar.WEEK_OF_MONTH, 2015);
-		// calendar.set(Calendar.MONTH, 0);
-		// calendar.set(Calendar.DAY_OF_MONTH, 1);
-		// System.out.println(calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT,
-		// Locale.CHINESE));
-		// System.out.println(calendar.get(Calendar.HOUR_OF_DAY));
+    public static void main(String[] args) {
+        final SimpleDateFormat sdf = new SimpleDateFormat("yyyy年 MM月 dd日");
+        System.out.println(sdf.format(new Date()));
 
-		// A a = new A();
-		// List<A> list = a.testA(a);// 杩欓噷澹版槑list鏃讹紝鎵�鐢ㄧ殑A鍗充細缁欎笅闈㈢殑testA鏂规硶绗竴涓猇璧嬪��
-		// System.out.println(list.get(0));
-		
-		String a = "{\"a\":\"abc\",\"b\":2}";
+        // Calendar calendar = Calendar.getInstance();
+        // calendar.set(Calendar.WEEK_OF_MONTH, 2015);
+        // calendar.set(Calendar.MONTH, 0);
+        // calendar.set(Calendar.DAY_OF_MONTH, 1);
+        // System.out.println(calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT,
+        // Locale.CHINESE));
+        // System.out.println(calendar.get(Calendar.HOUR_OF_DAY));
 
-		System.out.println(new Gson().fromJson(a, B.class));
-	}
+        // A a = new A();
+        // List<A> list = a.testA(a);// 杩欓噷澹版槑list鏃讹紝鎵�鐢ㄧ殑A鍗充細缁欎笅闈㈢殑testA鏂规硶绗竴涓猇璧嬪��
+        // System.out.println(list.get(0));
 
-	static class B {
-		private String a = "yyy";
-		private int b = 9999;
+        final String a = "{\"a\":\"abc\",\"b\":2}";
 
-		@Override
-		public String toString() {
-			return "B [a=" + a + ", b=" + b + "]";
-		}
-	}
+        final String b = "{" + a + "," + a + "}";
 
-	static class A {
-		// 绗竴涓猇鎵嶆槸鏂规硶閲屽彲浠ョ敤鐨勩�傜浜屼釜V鍙槸鐢ㄦ潵鎺у埗杩斿洖鍊肩殑绫诲瀷銆�
-		public <V> List<V> testA(V v) {
-			List<V> returnVlaue = new ArrayList<V>();
-			returnVlaue.add(v);
-			return returnVlaue;
-		}
-	}
+        System.out.println(b);
+
+        System.out.println(new Gson().fromJson(a, B.class));
+    }
+
+    static class B {
+        private final String a = "yyy";
+        private final int b = 9999;
+
+        @Override
+        public String toString() {
+            return "B [a=" + a + ", b=" + b + "]";
+        }
+    }
+
+    static class A {
+        // 绗竴涓猇鎵嶆槸鏂规硶閲屽彲浠ョ敤鐨勩�傜浜屼釜V鍙槸鐢ㄦ潵鎺у埗杩斿洖鍊肩殑绫诲瀷銆�
+        public <V> List<V> testA(V v) {
+            final List<V> returnVlaue = new ArrayList<V>();
+            returnVlaue.add(v);
+            return returnVlaue;
+        }
+    }
 }
