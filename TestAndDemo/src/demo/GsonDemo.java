d24d5beb2722faab1ddbfcf9e9bb1270af49ff5f
zhouxinxing <zhouxinxing@mobimagic.com>
2015-11-26 14:34:44 +0800

    fommater codes

--- TestAndDemo/src/demo/GsonDemo.java
+++ TestAndDemo/src/demo/GsonDemo.java
@@ -9,37 +9,37 @@ import com.google.gson.reflect.TypeToken;
 import com.sun.javafx.collections.MappingChange.Map;
 
 public class GsonDemo {
-	class Test {
-	String a;
-	String b;
+    class Test {
+        String a;
+        String b;
 
-	@Override
-	public String toString() {
-		return "Test [a=" + a + ", b=" + b + "]";
-	}
-	}
+        @Override
+        public String toString() {
+            return "Test [a=" + a + ", b=" + b + "]";
+        }
+    }
 
-	public static void main(String[] args) {
-	List<Object> list = new ArrayList<Object>();
-	list.add("2");
-	list.add("2");
-	list.add("4");
+    public static void main(String[] args) {
+        final List<Object> list = new ArrayList<Object>();
+        list.add("2");
+        list.add("2");
+        list.add("4");
 
-	List<String> list2 = new ArrayList<String>();
-	list2.add("a");
-	list2.add("b");
-	list.add(list2);
+        final List<String> list2 = new ArrayList<String>();
+        list2.add("a");
+        list2.add("b");
+        list.add(list2);
 
-	GsonBuilder gsonBuilder = new GsonBuilder();
-	Gson gson = gsonBuilder.create();
+        final GsonBuilder gsonBuilder = new GsonBuilder();
+        final Gson gson = gsonBuilder.create();
 
-	String jsonString = gson.toJson(list2);
+        final String jsonString = gson.toJson(list2);
 
-	// Test result = gson.fromJson("{\"a\":\"abc\",\"b\":\"123\"}", Test.class);
-	Map<String, String> result = gson.fromJson("{\"a\":\"abc\",\"b\":\"123\"}",
-		new TypeToken<Map<String, String>>() {
-		}.getType());
+        // Test result = gson.fromJson("{\"a\":\"abc\",\"b\":\"123\"}", Test.class);
+        final Map<String, String> result = gson.fromJson("{\"a\":\"abc\",\"b\":\"123\"}",
+                new TypeToken<Map<String, String>>() {
+                }.getType());
 
-	System.out.println(result);
-	}
+        System.out.println(result);
+    }
 }
--- TestAndDemo/src/demo/GuavaTest.java
+++ TestAndDemo/src/demo/GuavaTest.java
@@ -13,57 +13,57 @@ import com.google.common.collect.Multimap;
 import com.google.common.collect.Multimaps;
 
 public class GuavaTest {
-	public static void main(String[] args) {
-		// 按照指定key对应的value将map排序(强大的分片集合)
-		Map<String, String> map1 = ImmutableMap.of("1", "a", "2", "b", "3", "c");
-		Map<String, String> map2 = ImmutableMap.of("1", "a", "2", "b_", "3", "c_");
-		Map<String, String> map3 = ImmutableMap.of("1", "a_*", "2", "b_*", "3", "c_*");
+    public static void main(String[] args) {
+        // 按照指定key对应的value将map排序(强大的分片集合)
+        final Map<String, String> map1 = ImmutableMap.of("1", "a", "2", "b", "3", "c");
+        final Map<String, String> map2 = ImmutableMap.of("1", "a", "2", "b_", "3", "c_");
+        final Map<String, String> map3 = ImmutableMap.of("1", "a_*", "2", "b_*", "3", "c_*");
 
-		List<Map<String, String>> listOfMaps = Lists.newArrayList();
-		listOfMaps.add(map1);
-		listOfMaps.add(map2);
-		listOfMaps.add(map3);
+        final List<Map<String, String>> listOfMaps = Lists.newArrayList();
+        listOfMaps.add(map1);
+        listOfMaps.add(map2);
+        listOfMaps.add(map3);
 
-		Multimap<String, Map<String, String>> partitionedMap = Multimaps.index(listOfMaps, new Function<Map<String, String>, String>() {
-			@Override
-			public String apply(final Map<String, String> from) {
-				from.keySet();
-				return from.get("1");
-			}
-		});
+        final Multimap<String, Map<String, String>> partitionedMap = Multimaps.index(listOfMaps,
+                new Function<Map<String, String>, String>() {
+                    @Override
+                    public String apply(final Map<String, String> from) {
+                        from.keySet();
+                        return from.get("1");
+                    }
+                });
 
-		System.out.println("Multimaps.index: \n" + partitionedMap + "\n\n");
+        System.out.println("Multimaps.index: \n" + partitionedMap + "\n\n");
 
-		// Multimap： 1个key对应N个value
-		Multimap<String, String> customersByType = ArrayListMultimap.create();
-		customersByType.put("abc", "123");
-		customersByType.put("abc", "456");
-		customersByType.put("abc", "789");
-		customersByType.put("abc", "110");
-		customersByType.put("abcd", "220");
-		customersByType.put("abcde", "330");
+        // Multimap： 1个key对应N个value
+        final Multimap<String, String> customersByType = ArrayListMultimap.create();
+        customersByType.put("abc", "123");
+        customersByType.put("abc", "456");
+        customersByType.put("abc", "789");
+        customersByType.put("abc", "110");
+        customersByType.put("abcd", "220");
+        customersByType.put("abcde", "330");
 
-		System.out.println("Multimap：");
-		for (String item : customersByType.get("abc")) {
-			System.out.println(item);
-		}
-		System.out.println("\n");
+        System.out.println("Multimap：");
+        for (final String item : customersByType.get("abc")) {
+            System.out.println(item);
+        }
+        System.out.println("\n");
 
-		// MapDifference的一些用法
-		Map<String, String> mapA = ImmutableMap.of("1", "a", "2", "b", "3", "c", "22", "bb");
-		Map<String, String> mapB = ImmutableMap.of("1", "a", "2", "b_", "3", "c_");
+        // MapDifference的一些用法
+        final Map<String, String> mapA = ImmutableMap.of("1", "a", "2", "b", "3", "c", "22", "bb");
+        final Map<String, String> mapB = ImmutableMap.of("1", "a", "2", "b_", "3", "c_");
 
-		MapDifference<String, String> differenceMap = Maps.difference(mapA, mapB);
+        final MapDifference<String, String> differenceMap = Maps.difference(mapA, mapB);
 
-		Map entriesDiffering = differenceMap.entriesDiffering();// 同一个key,不同的value
-		Map entriesOnlyOnLeft = differenceMap.entriesOnlyOnLeft();// 只在左map存在的key
-		Map entriesOnlyOnRight = differenceMap.entriesOnlyOnRight();// 只在右map存在的key
-		Map entriesInCommon = differenceMap.entriesInCommon();// 同一个key,同一个value
+        final Map entriesDiffering = differenceMap.entriesDiffering();// 同一个key,不同的value
+        final Map entriesOnlyOnLeft = differenceMap.entriesOnlyOnLeft();// 只在左map存在的key
+        final Map entriesOnlyOnRight = differenceMap.entriesOnlyOnRight();// 只在右map存在的key
+        final Map entriesInCommon = differenceMap.entriesInCommon();// 同一个key,同一个value
 
-		System.out.println("MapDifference: \n" + differenceMap.entriesDiffering());
-		System.out.println(differenceMap.entriesOnlyOnLeft());
-		System.out.println(differenceMap.entriesOnlyOnRight());
-		System.out.println(differenceMap.entriesInCommon() + "\n\n");
-
-	}
+        System.out.println("MapDifference: \n" + differenceMap.entriesDiffering());
+        System.out.println(differenceMap.entriesOnlyOnLeft());
+        System.out.println(differenceMap.entriesOnlyOnRight());
+        System.out.println(differenceMap.entriesInCommon() + "\n\n");
+    }
 }
