611eed5f225b935a6bfcf18600aacff1bb90554c
zhouxinxing <zhouxinxing@mobimagic.com>
2015-12-17 18:34:55 +0800

    no message

--- TestAndDemo/src/demo/ThreadDemo.java
+++ TestAndDemo/src/demo/ThreadDemo.java
@@ -1,22 +1,25 @@
 package demo;
 
-public class TestDemo {
+public class ThreadDemo {
     public static void main(String[] args) {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                TestDemo.test(1);
+                ThreadDemo.test(1);
             }
         }).run();
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-                TestDemo.test(2);
+                ThreadDemo.test(2);
             }
         }).run();
     }
 
+    /**
+     * 该函数是synchronized的，所以一次只能有一个线程访问
+     */
     private synchronized static void test(int threadNum) {
         System.out.println(threadNum);
 
@@ -26,6 +29,6 @@ public class TestDemo {
             e.printStackTrace();
         }
 
-        TestDemo.test(threadNum);
+        ThreadDemo.test(threadNum);
     }
 }
